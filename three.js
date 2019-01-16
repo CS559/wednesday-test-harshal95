33d9a8d6aa0ff1f520beaf11100a2ce7355bbbcb
gleicher (surface) <gleicher@cs.wisc.edu>
2019-01-16 11:52:33 -0600

    got up to animation

--- three.js
+++ three.js
@@ -30,4 +30,159 @@ window.onload = function() {
      */
     let span01 = document.getElementById("box1-span");
     span01.innerHTML = "write my text in the JavaScript Program";
-}
+
+    // we can also change the styles of things
+    span01.style["backgroundColor"] = "#CEE";   // CEE = light cyan
+    span01.style["color"] = "#008";             // 008 = mid blue  
+    // note that I chose to write style["color"] rather than style.color
+    // this is a coding style thing.
+    // JavaScript pros prefer the dot notation (in fact, jshint will give a warning)
+    // but I did it this way since it emphasizes what's going on
+
+
+    /**
+     * Code for Box2
+     * This code does things with the buttons to show events
+     */
+
+     // get the first button, the first list element, and the corresponding text
+     // element
+    let button21 = document.getElementById("box2-button1");
+    let li21 = document.getElementById("box2-li1");
+    // when the button is clicked, change the text
+    button21.onclick = function() {
+        li21.innerText = "Button 1 was clicked!";
+    };
+
+    // for the second button, we can try the mouse down and mouse up events
+    let button22 = document.getElementById("box2-button2");
+    let li22 = document.getElementById("box2-li2");
+    // on mouse down, change the text
+    button22.onmousedown = function() {
+        li22.innerText = "Button 2 is being pressed - to break it move the mouse outside the button before releasing";
+    };
+    button22.onmouseup = function() {
+        li22.innerText = "Button 2 has been released";
+    };
+
+    // the problem is the we only get mouse up events if the cursor is over
+    // the button.
+    // there are lots of ways to deal with this - here's one using the
+    // mouseleave event (when the mouse leaves the button)
+    let button23 = document.getElementById("box2-button3");
+    let li23 = document.getElementById("box2-li3");
+    // I will keep track of state myself
+    let button23state = 0;
+    button23.onmousedown = function() {
+        li23.innerText = "Button 3 is being pressed";
+        button23state = 1;
+    };
+    button23.onmouseup = function() {
+        if (button23state) {
+            li23.innerText = "Button 3 has been released correctly";
+        } else {
+            li23.innerText = "Button 3 was released without being pressed (enter while press)";
+        }
+        button23state = 0;
+    };
+    button23.onmouseleave = function() {
+        if (button23state) {
+            li23.innerText = "Button 3 was left without being released";
+        } 
+        button23state = 0;
+    };
+
+
+    /**
+     * Code for Box3
+     * 
+     * Here, use the 3 sliders and catch different events
+     */
+    // note the weird comments to declare type information!
+    // these aren't necessary, but they can be useful for type checking
+    /** @type{HTMLInputElement} */ let slider31 = (/** @type{HTMLInputElement} */ document.getElementById("box3-slider1"));
+    /** @type{HTMLInputElement} */ let slider32 = (/** @type{HTMLInputElement} */ document.getElementById("box3-slider2"));
+    /** @type{HTMLInputElement} */ let slider33 = (/** @type{HTMLInputElement} */ document.getElementById("box3-slider3"));
+ 
+    // respond to different events on each
+    slider31.onchange = function() {
+        document.getElementById("box3-li1").innerHTML = `Slider 1 - <b>onchange</b> - new value ${slider31.value}`;
+    };
+    slider32.oninput = function() {
+        document.getElementById("box3-li2").innerHTML = `Slider 2 - <b>oninput</b> - new value ${slider32.value}`;
+    };
+    slider33.onclick = function() {
+        document.getElementById("box3-li3").innerHTML = `Slider 3 - <b>onclick</b> - new value ${slider33.value}`;
+    };
+
+
+    /**
+     * Code for Box 4
+     * 
+     * Have the sliders change each other
+     */
+    /** @type{HTMLInputElement} */ let slider41 = (/** @type{HTMLInputElement} */ document.getElementById("box4-slider1"));
+    /** @type{HTMLInputElement} */ let slider42 = (/** @type{HTMLInputElement} */ document.getElementById("box4-slider2"));
+    slider41.onchange = function() {
+        slider42.value = slider41.value;
+    };
+    slider42.oninput = function() {
+        slider41.value = slider42.value;
+    };
+
+    /**
+     * Code for Box 5
+     * 
+     * Request animation frame examples
+     * 
+     * Notice that I use the "performance.now" function to get the timing information
+     */
+    // use request animation frame to do something 16ms
+    // in the future - this kindof one off usage is a little
+    // weird
+    // basically, what this code is doing is scheduling an event for the future
+    // and then handling the event
+    let timestamp = performance.now();
+    window.requestAnimationFrame(function() {
+        let span = document.getElementById("box5-rs1");
+        span.innerText = "Hello from "+(performance.now()-timestamp)+"ms in the future";
+    });
+
+    let rs2 = document.getElementById("box5-rs2");
+    window.requestAnimationFrame(function() {
+        // this is the function for the first request
+        rs2.innerText = "Hello from First. ";
+        // remember when this happened
+        let ts1 = performance.now();
+        // now queue up the second one, even farther in the future
+        window.requestAnimationFrame(function() {
+            // this is the function forthe second request
+            // not that I am adding to innertext
+            let now = performance.now();
+            // remember that we can see the variable ts1 from the outer
+            // function (thanks to closure!)
+            rs2.innerText += " Hello from Second "+(now-ts1)+" ms after First.";
+        });
+    });
+
+    // a more typical usage of request animation frame
+    // a function schedules itself to be called in order to
+    // make a loop
+    /** @type{HTMLInputElement} */ let slr1 = (/** @type{HTMLInputElement} */ document.getElementById("box5-slider"));
+    function advanceSLR1() {
+        // add 1, roll over to zero if we hit the max
+        // note that the value of the slider is a string,
+        // so we have to convert it to a number
+        // The more obvious 1+"1" = "11" - thanks to JavaScripts
+        // aggressive coercion rules
+        let newValue = (Number(slr1.value)+1) % 100;
+        slr1.value = newValue.toString();
+        // ask for this to be called again 16ms in the future
+        window.requestAnimationFrame(advanceSLR1);
+    }
+    // note that just defined the function, now we need to call it
+    // to start the loop
+    advanceSLR1();
+
+
+};
